ee993d912e72e910b9743e3a0a5c15efc0df3407
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-02-12 18:49:26 -0800

    updates

---
+++ client/build/precache-manifest.fbace3d7a6373cd653a274e22c456a65.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b860b9cd50b5e8c00bf6",
+    "url": "/DKMS37/project3/static/css/main.5504c354.chunk.css"
+  },
+  {
+    "revision": "b860b9cd50b5e8c00bf6",
+    "url": "/DKMS37/project3/static/js/main.b860b9cd.chunk.js"
+  },
+  {
+    "revision": "2e40613ad379da1045c5",
+    "url": "/DKMS37/project3/static/js/1.2e40613a.chunk.js"
+  },
+  {
+    "revision": "5150af27208e663c115f",
+    "url": "/DKMS37/project3/static/js/runtime~main.5150af27.js"
+  },
+  {
+    "revision": "990d04eb6bdebd3072b2b332a214f326",
+    "url": "/DKMS37/project3/static/media/userpic.990d04eb.jpeg"
+  },
+  {
+    "revision": "fb5a22e7b8e55829571ad890a6b6438a",
+    "url": "/DKMS37/project3/static/media/followingpic1.fb5a22e7.jpg"
+  },
+  {
+    "revision": "ac1d2aa75335c3c431d81fdcb0617435",
+    "url": "/DKMS37/project3/static/media/followingpic2.ac1d2aa7.jpg"
+  },
+  {
+    "revision": "301cb7da14e681ba03f22cce998450a7",
+    "url": "/DKMS37/project3/static/media/followingpic3.301cb7da.png"
+  },
+  {
+    "revision": "d005484c601edc57e961ae311b5473af",
+    "url": "/DKMS37/project3/static/media/followingpic4.d005484c.jpg"
+  },
+  {
+    "revision": "d319a7754da9a24ccbd79eed6bf18f29",
+    "url": "/DKMS37/project3/static/media/followpic1.d319a775.jpg"
+  },
+  {
+    "revision": "b91715db567c17ce8fffd8f37d459ef8",
+    "url": "/DKMS37/project3/static/media/followpic3.b91715db.jpg"
+  },
+  {
+    "revision": "af31028a1334a11bbe7f3fd65e09cb39",
+    "url": "/DKMS37/project3/static/media/followpic4.af31028a.jpg"
+  },
+  {
+    "revision": "fa13005154d41d15faa3f0424a06cd31",
+    "url": "/DKMS37/project3/static/media/feed1.fa130051.jpeg"
+  },
+  {
+    "revision": "fe92b5b603a840c3df7c39bb9881466e",
+    "url": "/DKMS37/project3/static/media/feed2.fe92b5b6.jpeg"
+  },
+  {
+    "revision": "697563e5633c7c2a3ede2206d491e673",
+    "url": "/DKMS37/project3/static/media/feed3.697563e5.jpeg"
+  },
+  {
+    "revision": "ffb97d9aabe4450d61355af4f1a09902",
+    "url": "/DKMS37/project3/index.html"
+  }
+];
